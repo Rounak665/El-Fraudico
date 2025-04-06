50f3f37cd9140cdc4099fa40b7565a6263ab7373
Rounak <168360763+Rounak665@users.noreply.github.com>
2025-04-06 20:01:52 +0530

    Added Backend

---
+++ backend/src/main/java/com/football/elfraudico/ElFraudicoApplication.java
@@ -0,0 +1,13 @@
+package com.football.elfraudico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElFraudicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElFraudicoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/football/elfraudico/ElFraudicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.elfraudico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElFraudicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
